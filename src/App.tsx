202ff58690358c38cb2190df87474814ce66d272
Konstantin Kiselev <dimamid99@gmail.com>
2023-08-03 19:26:16 +0500

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,80 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, {useEffect, useState} from 'react';
+import {getMailboxName, getIncomingEmailStream, IncomingEmail, createIncomingEmailStream} from './api';
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React + Custom message</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
 
-export default App
+const App: React.FC = () => {
+    const [mailboxName, setMailboxName] = useState<string | null>(null);
+    const [incomingEmails, setIncomingEmails] = useState<string[]>([]);
+
+    useEffect(() => {
+        // Fetch mailbox name on component mount
+        fetchMailboxName().then();
+
+        createStream().then();
+        // Open the event stream for incoming emails
+        // fetchIncomingEmails().then();
+
+        const eventSource = new EventSource('http://localhost:5173/stream?stream=123');
+
+        eventSource.onmessage = (event) => {
+            console.log("received new message")
+            const newEmail = JSON.parse(event.data);
+            setIncomingEmails((prevEmails) => [event.data, ...prevEmails]);
+        };
+
+        return () => {
+            eventSource.close();
+        };
+    }, []);
+
+    const fetchMailboxName = async () => {
+        try {
+            console.log('fetching mailbox name')
+            const name = await getMailboxName();
+            console.log(name)
+            setMailboxName(name.email);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    const createStream = async () => {
+        try {
+            console.log('creating stream')
+            const stream = await createIncomingEmailStream();
+            console.log(stream)
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    // const fetchIncomingEmails = async () => {
+    //     try {
+    //         console.log('fetching emails')
+    //         const emails = await getIncomingEmailStream();
+    //         console.log(emails)
+    //         setIncomingEmails((prevEmails) => [...prevEmails, emails.result]);
+    //     } catch (error) {
+    //         console.error(error);
+    //     }
+    // }
+
+    return (
+        <div>
+            <h1>Temporary Mailbox</h1>
+            {mailboxName && <p>Mailbox Name: {mailboxName}</p>}
+            <h2>Incoming Emails</h2>
+            <ul>
+                {incomingEmails.map((message, index) => (
+                    <li key={index}>
+                        <strong>EMail: </strong>
+                        {message}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    server: {
+        cors: false,
+        proxy: {
+            '/api/pigeomail/v1': {
+                target: 'http://localhost:20202/',
+                changeOrigin: true,
+                secure: false,
+            },
+            '/stream': {
+                target: 'http://localhost:8080/',
+                changeOrigin: true,
+                secure: false,
+            },
+            '/create_stream': {
+                target: 'http://localhost:8080/',
+                changeOrigin: true,
+                secure: false,
+            }
+        }
+    }
 })
